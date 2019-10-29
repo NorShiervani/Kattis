5cbf99fc13fb430e4bdfbbdf89a066b83266b771
NorShiervani <50927017+NorShiervani@users.noreply.github.com>
2019-10-29 19:51:01 +0100

    Reversed Binary Numbers - Difficulty: 1.4

---
+++ reversebinary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    class reversebinary
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            char[] binary = Convert.ToString(number, 2).ToCharArray();
+            Array.Reverse(binary);
+            string reversedBinary = new string(binary);
+            int reversedNumber = Convert.ToInt32(reversedBinary, 2);
+
+            Console.WriteLine(reversedNumber);
+            Console.ReadLine();
+        }
+    }
+}
