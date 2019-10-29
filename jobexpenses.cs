3494f9a0d965dabe050a2a6f220b8580f75cc541
NorShiervani <50927017+NorShiervani@users.noreply.github.com>
2019-10-29 10:38:44 +0100

    Job Expenses - Difficulty: 1.5

---
+++ jobexpenses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kattis
+{
+    class jobexpenses
+    {
+        static void Main(string[] args)
+        {
+            int amountOfNumbers = int.Parse(Console.ReadLine());
+            int[] numbers = new int[amountOfNumbers];
+            numbers = Console.ReadLine().Split(' ').Select(int.Parse).Take(numbers.Length).ToArray();
+            int totalExpenses = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number < 0)
+                    totalExpenses += Math.Abs(number);
+            }
+
+            Console.WriteLine(totalExpenses);
+            Console.ReadLine();
+        }
+    }
+}
